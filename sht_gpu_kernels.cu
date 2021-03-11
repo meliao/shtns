cb48a2c46f4713d7c2484390f19428b9b90bb669
Nathanaël Schaeffer <nathanael.schaeffer@gmail.com>
2021-03-11 23:19:54 +0100

    cuda: fix normalization issue of analysis when using SHT_REAL_NORM

--- sht_gpu_kernels.cu
+++ sht_gpu_kernels.cu
@@ -1538,7 +1538,7 @@ static void leg_m(shtns_cfg shtns, const double *ql, double *q, const int llim, 
 }
 
 template<int BLOCKSIZE, int LSPAN, int S, int NFIELDS, bool HI_LLIM> __global__ void
-ileg_m_kernel(const double* __restrict__ al, const double* __restrict__ ct, const double* __restrict__ q, double *ql, const int llim, const int nlat_2, const int lmax, const int mres, const int nphi, const int q_dist=0, const int ql_dist=0)
+ileg_m_kernel(const double* __restrict__ al, const double* __restrict__ ct, const double* __restrict__ q, double *ql, const int llim, const int nlat_2, const int lmax, const int mres, const int nphi, const double mpos_scale, const int q_dist=0, const int ql_dist=0)
 {
 	const int it = BLOCKSIZE * blockIdx.x + threadIdx.x;
 	const int j = threadIdx.x;
@@ -1737,7 +1737,7 @@ ileg_m_kernel(const double* __restrict__ al, const double* __restrict__ ct, cons
 		#ifdef SHTNS_ISHIOKA
 		cost = y0;		// cos(theta)^2
 		#endif
-		y0 = 0.5;				// y0
+		y0 = mpos_scale;	// y0
 		l = m - S;
 		int ny = 0;
 		int nsint = 0;
@@ -1904,7 +1904,7 @@ static void ileg_m(shtns_cfg shtns, const double* q, double *ql, const int llim,
 	if (llim < mmax*mres) mmax = llim / mres;	// truncate mmax too !
 	dim3 blocks(blocksPerGrid, mmax+1);
 	dim3 threads(threadsPerBlock, 1);
-	ileg_m_kernel<BLOCKSIZE, LSPAN_, S, NFIELDS, HI_LLIM><<<blocks, threads, 0, stream>>>(d_alm, d_ct, (double*) q, (double*) ql, llim, nlat_2, lmax,mres, nphi, q_dist, ql_dist);
+	ileg_m_kernel<BLOCKSIZE, LSPAN_, S, NFIELDS, HI_LLIM><<<blocks, threads, 0, stream>>>(d_alm, d_ct, (double*) q, (double*) ql, llim, nlat_2, lmax,mres, nphi, shtns->mpos_scale_analys, q_dist, ql_dist);
 }
 
 template<int S, int NFIELDS>
