meliao/shtns

commit b0a4ca0f4fb4824db6ae849bdc0396bf7e8575c5

batch mode: change spatial data layout so that the batch is the outermost dimension.
On GPU, this results in much better VkFFT performance, especially on AMD GPU.
shtns_set_batch() is renamed to shtns_set_many() to mark this change.

sht_gpu.cu
@@ -173,21 +173,20 @@ static int init_cuda_buffer_fft(shtns_cfg shtns, int cuda_gpu_id, int sizeof_rea
 			VkFFTConfiguration config = {};		//zero-initialize configuration
 			if (shtns->fft_mode & FFT_THETA_CONTIG) {
 				printf("!!! Use theta-contiguous FFT on GPU !!!\n");
-				long howmany = shtns->nlat_2 * shtns->howmany;		// support batched transforms
 				long dist = shtns->nlat_padded / 2;
 				config.FFTdim = 2; //FFT dimension: 1D, but we use a second dimension to get non-unit strides.
-				config.size[0] = howmany;
+				config.size[0] = shtns->nlat_2;
 				config.size[1] = nfft;
 				config.bufferStride[0] = dist;
 				config.bufferStride[1] = dist * nfft;
 				config.omitDimension[0] = 1;		// no FFT on the first dimension.
-				config.doublePrecision = sizeof_real / 8;
 				if (2*(shtns->mmax+1) <= nfft) {	// let vkFFT perform the zero-padding (saves memory bandwidth)
 					config.performZeropadding[1] = 1;
 					config.frequencyZeroPadding = 1;
 					config.fft_zeropad_left[1] = shtns->mmax + 1;			// first zero element
 					config.fft_zeropad_right[1] = nfft - shtns->mmax;		// first non-zero element
 				}
+				config.numberBatches = shtns->howmany;
 			} else if (shtns->fft_mode & FFT_PHI_CONTIG) {
 				printf("!!! Use phi-contiguous FFT on GPU (with transpose step) !!!\n");
 				long howmany = shtns->nlat * shtns->howmany;		// support batched transforms
@@ -233,7 +232,7 @@ static int init_cuda_buffer_fft(shtns_cfg shtns, int cuda_gpu_id, int sizeof_rea
 	const long nlm2 = shtns->nlm + (shtns->mmax+1);		// one more data per m
 	const long nphi = shtns->nphi;
 	const size_t nlm_stride = ((2*nlm2+WARPSZE-1)/WARPSZE) * WARPSZE;
-	const size_t spat_stride = ((shtns->nlat_padded*(nphi + (nphi/2==shtns->mmax))+WARPSZE-1)/WARPSZE) * WARPSZE;		// for odd nphi, reserve more space to store the full Fourier data
+	const size_t spat_stride = ((shtns->nlat_padded*(nphi + (nphi/2==shtns->mmax))+WARPSZE-1)/WARPSZE) * WARPSZE * howmany;		// for odd nphi, reserve more space to store the full Fourier data
 	const size_t dual_stride = (spat_stride < nlm_stride*howmany) ? nlm_stride*howmany : spat_stride;		// we need two spatial buffers to also hold spectral data.
 
 	size_t sze = nlm_stride;		// 1 spectral buffer for scalar only ...
@@ -765,8 +764,14 @@ void fourier_to_spat_gpu(shtns_cfg shtns, void* q, const int mmax, const long si
 			// THETA_CONTIGUOUS
 			// rely on vkfft to avoid reading the unused Fourier modes above shtns->mmax
 			if (mmax < shtns->mmax) {	// some zero must be added, only if more than nominal
-				const long nlat = shtns->nlat_padded;
-				cudaMemsetAsync( ((char*)q) + sizeof_real*(mmax+1)*nlat, 0, sizeof_real*(nphi-2*mmax-1)*nlat, shtns->comp_stream );		// zero out m>mmax before fft
+				const long nlat = shtns->nlat;
+				const long width = sizeof_real*nlat*(nphi-2*mmax-1);
+				char *dst = ((char*)q) + sizeof_real*nlat*(mmax+1);
+				if (shtns->howmany == 1) {
+					cudaMemsetAsync( dst, 0, width, shtns->comp_stream );		// zero out m>mmax before fft
+				} else {
+					cudaMemset2DAsync( dst, sizeof_real*shtns->spat_dist, 0, width, shtns->howmany, shtns->comp_stream );		// zero out m>mmax before fft
+				}
 			}
 			launchParams.buffer = (void**) &q;
 		}
@@ -819,7 +824,7 @@ static void legendre(shtns_cfg shtns, const int S, const void *ql, void *q, cons
 	int par_idx = (sh2ish_fuse) ? 2 : 0;
 
 	int llim_ = llim;
-	void* params[11] = {&shtns->d_clm, &shtns->d_ct, &ql, &q, &llim_, &nlat_2, &shtns->nphi, &shtns->nlat_padded, &nlm_stride, &shtns->nlat, &shtns->d_xlm};
+	void* params[11] = {&shtns->d_clm, &shtns->d_ct, &ql, &q, &llim_, &nlat_2, &shtns->nphi, &shtns->nlat_padded, &nlm_stride, &shtns->spat_dist, &shtns->d_xlm};
 	cuLaunchKernel(shtns->gpu_kernels[S], 
 			shtns->gridDim_x[par_idx], shtns->gridDim_y[0], mmax+1,		// grid dim
 			shtns->nwarp[par_idx]*WARPSZE, 1, 1,					// block dim
@@ -844,7 +849,7 @@ static void ilegendre(shtns_cfg shtns, const int S, const void *q, void* ql, con
 
 	int llim_ = llim;
 	float w_norm_1_f = shtns->wg[-1];	// convert to float
-	void* params[11] = {&shtns->d_clm, &shtns->d_ct, &q, &ql, &llim_, &nlat_2, &shtns->nphi, &shtns->nlat_padded, &shtns->nlat, &shtns->nlm_stride, &(shtns->wg[-1])};
+	void* params[11] = {&shtns->d_clm, &shtns->d_ct, &q, &ql, &llim_, &nlat_2, &shtns->nphi, &shtns->nlat_padded, &shtns->spat_dist, &shtns->nlm_stride, &(shtns->wg[-1])};
 	if (shtns->sizeof_real == 4) params[10] = &w_norm_1_f;		// weight_norm_1 as a float
 	cuLaunchKernel(shtns->gpu_kernels[2+S], 		// analysis kernels
 			shtns->gridDim_x[1], shtns->gridDim_y[1], mmax+1,		// grid dim

SHT/fly_SH_to_spat.gen.c
@@ -96,7 +96,7 @@ VX		BpF = BtF + nv/2;
 		{
 			for (int b=0; b<shtns->howmany; b++) {		// inner-loop is batch. For best cache-reuse
 				long spec_ofs = b * shtns->spec_dist;
-				long spat_ofs = b * shtns->nlat_2;
+				long spat_ofs = b * shtns->spat_dist/2;
 3				GEN3(_sy3,NWAY,_l)(shtns, Qlm+spec_ofs, Slm+spec_ofs, Tlm+spec_ofs, BrF+spat_ofs, BtF+spat_ofs, BpF+spat_ofs, llim, im, it0, it1);
 QX				GEN3(_sy1,NWAY,_l)(shtns, Qlm+spec_ofs, BrF+spat_ofs, llim, im, it0, it1);
 	#ifndef SHT_GRAD
@@ -114,7 +114,7 @@ T				GEN3(_sy1t,NWAY,_l)(shtns, Tlm+spec_ofs, BtF+spat_ofs, BpF+spat_ofs, llim, 
 		{
 			for (int b=0; b<shtns->howmany; b++) {		// inner-loop is batch. For best cache-reuse
 				long spec_ofs = b * shtns->spec_dist;
-				long spat_ofs = b * shtns->nlat_2;
+				long spat_ofs = b * shtns->spat_dist/2;
 3				GEN3(_sy3_hi,NWAY,_l)(shtns, Qlm+spec_ofs, Slm+spec_ofs, Tlm+spec_ofs, BrF+spat_ofs, BtF+spat_ofs, BpF+spat_ofs, llim, im, it0, it1);
 QX				GEN3(_sy1_hi,NWAY,_l)(shtns, Qlm+spec_ofs, BrF+spat_ofs, llim, im, it0, it1);
 	#ifndef SHT_GRAD
@@ -130,9 +130,12 @@ T				GEN3(_sy1t_hi,NWAY,_l)(shtns, Tlm+spec_ofs, BtF+spat_ofs, BpF+spat_ofs, lli
 	// padding for high m's
 	if (NPHI-1 > 2*imlim) {
 		const int m_inc = shtns->nlat_padded >> 1;
-Q		memset(BrF + m_inc*(imlim+1), 0, sizeof(cplx)* m_inc * (NPHI-1-2*imlim));
-V		memset(BtF + m_inc*(imlim+1), 0, sizeof(cplx)* m_inc * (NPHI-1-2*imlim));
-V		memset(BpF + m_inc*(imlim+1), 0, sizeof(cplx)* m_inc * (NPHI-1-2*imlim));
+		for (int b=0; b<shtns->howmany; b++) {
+			long spat_ofs = b * shtns->spat_dist/2;
+Q			memset(BrF + spat_ofs + m_inc*(imlim+1), 0, sizeof(cplx)* m_inc * (NPHI-1-2*imlim));
+V			memset(BtF + spat_ofs + m_inc*(imlim+1), 0, sizeof(cplx)* m_inc * (NPHI-1-2*imlim));
+V			memset(BpF + spat_ofs + m_inc*(imlim+1), 0, sizeof(cplx)* m_inc * (NPHI-1-2*imlim));
+		}
 	}
 
     // NPHI > 1 as SHT_AXISYM is not defined.

SHT/fly_spat_to_SH.gen.c
@@ -75,7 +75,7 @@ V			fftw_execute_split_dft(shtns->fftc, Vp+NPHI, Vp, BpF+1, BpF);
 		for (int im=0; im<=imlim; im++) {
 			for (int b=0; b<shtns->howmany; b++) {
 				long spec_ofs = b * shtns->spec_dist;
-				long spat_ofs = b * shtns->nlat;
+				long spat_ofs = b * shtns->spat_dist;
 QX				GEN3(_an1_hi,NWAY,_l)(shtns, BrF+spat_ofs, Qlm+spec_ofs, llim, im);
 VX				GEN3(_an2_hi,NWAY,_l)(shtns, BtF+spat_ofs, BpF+spat_ofs, Slm+spec_ofs, Tlm+spec_ofs, llim, im);
 3				GEN3(_an3_hi,NWAY,_l)(shtns, BrF+spat_ofs, BtF+spat_ofs, BpF+spat_ofs, Qlm+spec_ofs, Slm+spec_ofs, Tlm+spec_ofs, llim, im);
@@ -87,7 +87,7 @@ VX				GEN3(_an2_hi,NWAY,_l)(shtns, BtF+spat_ofs, BpF+spat_ofs, Slm+spec_ofs, Tlm
 		for (int im=0; im<=imlim; im++) {
 			for (int b=0; b<shtns->howmany; b++) {
 				long spec_ofs = b * shtns->spec_dist;
-				long spat_ofs = b * shtns->nlat;
+				long spat_ofs = b * shtns->spat_dist;
 QX				GEN3(_an1,NWAY,_l)(shtns, BrF+spat_ofs, Qlm+spec_ofs, llim, im);
 VX				GEN3(_an2,NWAY,_l)(shtns, BtF+spat_ofs, BpF+spat_ofs, Slm+spec_ofs, Tlm+spec_ofs, llim, im);
 3				GEN3(_an3,NWAY,_l)(shtns, BrF+spat_ofs, BtF+spat_ofs, BpF+spat_ofs, Qlm+spec_ofs, Slm+spec_ofs, Tlm+spec_ofs, llim, im);

SHT/omp_SH_to_spat.gen.c
@@ -85,7 +85,7 @@ VX		BpF = BtF + nv/2;
 	{
 	    for (int b=0; b<shtns->howmany; b++) {		// inner-loop is batch. For best cache-reuse
 		long spec_ofs = b * shtns->spec_dist;
-		long spat_ofs = b * shtns->nlat_2;
+		long spat_ofs = b * shtns->spat_dist/2;
 3		GEN3(_sy3_hi,NWAY,SUFFIX)(shtns, Qlm+spec_ofs, Slm+spec_ofs, Tlm+spec_ofs, BrF+spat_ofs, BtF+spat_ofs, BpF+spat_ofs, llim, im, it0, it1);
 QX		GEN3(_sy1_hi,NWAY,SUFFIX)(shtns, Qlm+spec_ofs, BrF+spat_ofs, llim, im, it0, it1);
 	#ifndef SHT_GRAD
@@ -103,9 +103,12 @@ T		GEN3(_sy1t_hi,NWAY,SUFFIX)(shtns, Tlm+spec_ofs, BtF+spat_ofs, BpF+spat_ofs, l
 		const int m_inc = shtns->nlat_padded >> 1;
 		#pragma omp for schedule(dynamic,1) nowait
 		for (int im=imlim+1; im < NPHI-imlim; im++)  {
-Q			memset(BrF + m_inc*im, 0, sizeof(cplx)* m_inc );
-V			memset(BtF + m_inc*im, 0, sizeof(cplx)* m_inc );
-V			memset(BpF + m_inc*im, 0, sizeof(cplx)* m_inc );
+		    for (int b=0; b<shtns->howmany; b++) {
+			long spat_ofs = b * shtns->spat_dist/2;
+Q			memset(BrF + spat_ofs + m_inc*im, 0, sizeof(cplx)* m_inc );
+V			memset(BtF + spat_ofs + m_inc*im, 0, sizeof(cplx)* m_inc );
+V			memset(BpF + spat_ofs + m_inc*im, 0, sizeof(cplx)* m_inc );
+		    }
 		}
 	}
   #endif

SHT/omp_spat_to_SH.gen.c
@@ -77,7 +77,7 @@ V			fftw_execute_split_dft(shtns->fftc, Vp+NPHI, Vp, BpF+1, BpF);
 			for (int im=0; im<=imlim; im++) {
 				for (int b=0; b<shtns->howmany; b++) {
 					long spec_ofs = b * shtns->spec_dist;
-					long spat_ofs = b * shtns->nlat;
+					long spat_ofs = b * shtns->spat_dist;
 QX					GEN3(_an1,NWAY,SUFFIX)(shtns, BrF+spat_ofs, Qlm+spec_ofs, llim, im);
 VX					GEN3(_an2,NWAY,SUFFIX)(shtns, BtF+spat_ofs, BpF+spat_ofs, Slm+spec_ofs, Tlm+spec_ofs, llim, im);
 3					GEN3(_an3,NWAY,SUFFIX)(shtns, BrF+spat_ofs, BtF+spat_ofs, BpF+spat_ofs, Qlm+spec_ofs, Slm+spec_ofs, Tlm+spec_ofs, llim, im);
@@ -88,7 +88,7 @@ VX					GEN3(_an2,NWAY,SUFFIX)(shtns, BtF+spat_ofs, BpF+spat_ofs, Slm+spec_ofs, T
 			for (int im=0; im<=imlim; im++) {
 				for (int b=0; b<shtns->howmany; b++) {
 					long spec_ofs = b * shtns->spec_dist;
-					long spat_ofs = b * shtns->nlat;
+					long spat_ofs = b * shtns->spat_dist;
 QX					GEN3(_an1_hi,NWAY,SUFFIX)(shtns, BrF+spat_ofs, Qlm+spec_ofs, llim, im);
 VX					GEN3(_an2_hi,NWAY,SUFFIX)(shtns, BtF+spat_ofs, BpF+spat_ofs, Slm+spec_ofs, Tlm+spec_ofs, llim, im);
 3					GEN3(_an3_hi,NWAY,SUFFIX)(shtns, BrF+spat_ofs, BtF+spat_ofs, BpF+spat_ofs, Qlm+spec_ofs, Slm+spec_ofs, Tlm+spec_ofs, llim, im);

cuda2hip.h
@@ -21,6 +21,7 @@
 #define cudaMemcpyDeviceToHost	 hipMemcpyDeviceToHost
 #define cudaMemcpyHostToDevice	 hipMemcpyHostToDevice
 #define cudaMemsetAsync	 hipMemsetAsync
+#define cudaMemset2DAsync	 hipMemset2DAsync
 #define cudaSetDevice	 hipSetDevice
 #define cudaStreamCreateWithFlags	 hipStreamCreateWithFlags
 #define cudaStreamDestroy	 hipStreamDestroy

cuda2hip_gen.py
@@ -40,6 +40,7 @@ cudaMemcpyAsync
 cudaMemcpyDeviceToHost
 cudaMemcpyHostToDevice
 cudaMemsetAsync
+cudaMemset2DAsync
 cudaSetDevice
 cudaStreamCreateWithFlags
 cudaStreamDestroy

sht_init.c
@@ -400,22 +400,23 @@ static void planFFT(shtns_cfg shtns, int layout)
 	#endif
 
 	// default layout:
-	phi_inc = shtns->nlat * howmany;
+	phi_inc = shtns->nlat;
 	if (layout & SHT_ALLOW_PADDING) {	// handle padding
 		int pad = 0;
 		#ifndef SHTNS_GPU
-		if ((phi_inc % 64 == 0) && (NPHI * phi_inc > 512) && ((NPHI>1)||(howmany>1)))
+		if ((phi_inc % 64 == 0) && (NPHI * phi_inc > 512) && (NPHI>1))
 			pad = 8;			// we add some padding, to avoid cache bank conflicts.
 		#elif SHTNS_GPU==2
-		if ((phi_inc % 256 == 0) && (NPHI * phi_inc > 4096) && (NPHI>1))
-			pad = 8;			// add padding to avoid memory bank / channel conflicts on AMD GPUs.
+		if ((phi_inc % 32 == 0) && (NPHI * phi_inc > 4096) && (NPHI>1))
+			pad = 8;		// add padding to avoid memory bank / channel / whatever conflicts on AMD GPUs (large impact on performance).
 		#endif
 		const char* env_pad = getenv("SHTNS_PAD");    if (env_pad) pad = atoi(env_pad);		// override default with SHTNS_PAD environment variable
 		phi_inc += pad;
 	}
 	shtns->k_stride_a = 1;		shtns->m_stride_a = phi_inc;		// default strides
 	shtns->nlat_padded = phi_inc;		// stride between phi in spectral domain
-	shtns->nspat = NPHI * phi_inc;		// default spatial size to be allocated for a transform call
+	shtns->nspat = NPHI * phi_inc * howmany;		// default spatial size to be allocated for a transform call
+	shtns->spat_dist = NPHI * phi_inc;
 
 	if (NPHI==1) 	// no FFT needed.
 	{
@@ -503,7 +504,15 @@ static void planFFT(shtns_cfg shtns, int layout)
 		if ((NLAT & 1)==0) {
 			if (verbose) printf("(theta-contiguous layout: phi_inc=%d, theta_inc=%d)\n",phi_inc,theta_inc);
 			shtns->fft_mode = FFT_THETA_CONTIG;
-			shtns->ifftc = fftw_plan_many_dft(1, &nfft, shtns->nlat_2 * howmany, ShF, &nfft, phi_inc/2, 1, ShF, &nfft, phi_inc/2, 1, FFTW_BACKWARD, shtns->fftw_plan_mode);
+			if (howmany==1) {
+				shtns->ifftc = fftw_plan_many_dft(1, &nfft, shtns->nlat_2 * howmany, ShF, &nfft, phi_inc/2, 1, ShF, &nfft, phi_inc/2, 1, FFTW_BACKWARD, shtns->fftw_plan_mode);
+			} else {
+				fftw_iodim dim, many[2];
+				dim.n = NPHI;    		dim.os = shtns->nlat_padded/2;		dim.is = shtns->nlat_padded/2;
+				many[0].n = NLAT/2;		many[0].os = 1;				many[0].is = 1;
+				many[1].n = howmany;	many[1].os = shtns->nlat_padded/2 * NPHI;	many[1].is = shtns->nlat_padded/2 * NPHI;
+				shtns->ifftc = fftw_plan_guru_dft(1, &dim, 2, many, ShF, ShF, FFTW_BACKWARD, shtns->fftw_plan_mode);
+			}
 			shtns->fftc = shtns->ifftc;		// same thing, with m>0 and m<0 exchanged.
 
 		/*	if (shtns->nthreads > 1) {
@@ -1116,7 +1125,7 @@ shtns_cfg shtns_create(int lmax, int mmax, int mres, enum shtns_norm norm)
 		#else
 		shtns->robert_form = 0;		// no Robert form by default.
 		#endif
-		shtns->howmany = 1;		// 1 transform by default. Use shtns_set_batch() to ask for more.
+		shtns->howmany = 1;		// 1 transform by default. Use shtns_set_many() to ask for more.
 		shtns->cpu_timer = -1;		// disable timing by default
 	}
 
@@ -1542,18 +1551,19 @@ int shtns_set_grid(shtns_cfg shtns, enum shtns_type flags, double eps, int nlat,
  * Currently only theta-contiguous data is allowed.
  * This function must be called before \ref shtns_set_grid or \ref shtns_set_grid_auto, after which the spatial datat layout will be defined
  * by \c shtns->nlat_padded and \c shtns->nspat as:
- * \code data[i_phi*shtns->nlat_padded + i_batch*shtns->nlat + i_theta] \endcode
+ * \code data[(i_batch*shtns->nphi + i_phi)*shtns->nlat_padded + i_theta] \endcode
  * Note that \c shtns->nspat will be the number of spatial points in howmany fields (not in a single field).
  * \param[in] shtns = a plan created by \ref shtns_create that should handle many transforms at once.
  * \param[in] howmany = number of transforms in batch.
  * \param[in] spec_dist = distance between spectral arrays in batch. Spectral data is accessed with \code Qlm[i_batch * spec_dist + lm] \endcode
  * \returns howmany on success, or -1 on failure.
 */
-int shtns_set_batch(shtns_cfg shtns, const int howmany, long spec_dist)
+int shtns_set_many(shtns_cfg shtns, const int howmany, long spec_dist)
 {
 	if (howmany <= 0)	return -1;		// invalid
 	if (spec_dist == 0)  spec_dist = shtns->nlm;
 	if (spec_dist < shtns->nlm) return -1;	// invalid
+	if (shtns->nspat != 0) return -1;		// grid already set!!
 
 	shtns->howmany = howmany;
 	shtns->spec_dist = spec_dist;		// distance between spectral fields.

commit 3a2fbfbb4009ef2ff0efbfb2688171975df84083

GPU: fix error on AMD GPU, introduced in commit b0a4ca0f leading to wrong synthesis when both padding and truncating at lower m are required.

sht_gpu.cu
@@ -764,7 +764,7 @@ void fourier_to_spat_gpu(shtns_cfg shtns, void* q, const int mmax, const long si
 			// THETA_CONTIGUOUS
 			// rely on vkfft to avoid reading the unused Fourier modes above shtns->mmax
 			if (mmax < shtns->mmax) {	// some zero must be added, only if more than nominal
-				const long nlat = shtns->nlat;
+				const long nlat = shtns->nlat_padded;
 				const long width = sizeof_real*nlat*(nphi-2*mmax-1);
 				char *dst = ((char*)q) + sizeof_real*nlat*(mmax+1);
 				if (shtns->howmany == 1) {